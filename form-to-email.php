3171d3095403b4295d4e325392ffc13a1bd8acb5
Josh Rehm <jrehm135@vt.edu>
2017-03-04 15:32:34 -0500

    trying get

--- form-to-email.php
+++ form-to-email.php
@@ -1,12 +1,12 @@
 <?php
-if(!isset($_POST['submit']))
+if(!isset($_GET['submit']))
 {
 	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$year = $_POST['year'];
+$name = $_GET['name'];
+$visitor_email = $_GET['email'];
+$year = $_GET['year'];
 
 //Validate first
 if(empty($name)||empty($visitor_email)) 
@@ -26,9 +26,8 @@ $email_subject = "Rushee Information";
 $email_body = "Recruitment page visit from $name.\n".
     "They are a $year".
     
-$to = "donaldriblet@gmail.com";
+$to = "jrehm135@gmail.com";
 $headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
